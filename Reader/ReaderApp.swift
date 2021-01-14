16e19ddd0c8b41533c38b507b56399509af53c24
Legolas <057@live.com>
2021-01-13 19:51:57 -0800

    Initial Commit

---
+++ Reader/ReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReaderApp.swift
+//  Reader
+//
+//  Created by Legolas on 2021-01-13.
+//
+
+import SwiftUI
+
+@main
+struct ReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

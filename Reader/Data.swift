07f39bbb51a1e723dc5b250712077b760335b6bf
Legolas <057@live.com>
2021-01-19 13:12:26 -0800

    DataUpdate

---
+++ Reader/Data.swift
@@ -0,0 +1,39 @@
+//
+//  Data.swift
+//  Reader
+//
+//  Created by Legolas on 2021-01-13.
+//
+
+import SwiftUI
+
+// Model
+struct Article: Codable, Identifiable {
+    var id: Int
+    var title: String
+    var body: String
+}
+
+
+// ViewModel
+class Data: ObservableObject {
+    @Published var articles = [Article]()
+    
+    init() {
+        let url = URL(string: "https://www.legolas.me/s/articles.json")!
+        
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            DispatchQueue.main.async {
+                self.articles = try! JSONDecoder().decode([Article].self, from: data!)
+            }
+        }.resume()
+    }
+}
+
+
+
+
+
+
+
+

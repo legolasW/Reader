f3611bb384072bc758bd5523019076d35514b389
Legolas <057@live.com>
2021-02-09 15:54:34 -0800

    Image Picker added.

--- Reader/ViewModel + Model/TabNoteData.swift
+++ Reader/ViewModel + Model/TabNoteData.swift
@@ -4,13 +4,14 @@
 //
 //  Created by Legolas on 2021-02-01.
 //
-import Foundation
+import SwiftUI
 
 // Model
 struct Note: Codable, Identifiable {
     var id: UUID
     var title: String
     var content: String
+    var imageURLAppendix: String?
 }
 
 // ModelView
@@ -22,6 +23,19 @@ class TabNoteData: ObservableObject {
     
     init() {
         notes = getNotes()
+    }
+    
+    func getImage(_ imageURLAppendix: String) -> UIImage {
+        let url = TabNoteData.sandboxURL.appendingPathComponent(imageURLAppendix)
+        let imageData = try! Data(contentsOf: url)
+        return UIImage(data: imageData, scale: 0.5)!
+    }
+    
+    func saveImage(id: UUID, data: Data) {
+        DispatchQueue.global(qos: .userInitiated).async {
+            let url = TabNoteData.sandboxURL.appendingPathComponent("\(id).png")
+            try? data.write(to: url)
+        }
     }
     
     func getNotes() -> [Note] {
--- Reader/Views/NoteEntryAndList.swift
+++ Reader/Views/NoteEntryAndList.swift
@@ -6,29 +6,49 @@
 //
 
 import SwiftUI
+import ImagePickerView
 
 struct NoteEntryAndList: View {
     @StateObject var noteData = TabNoteData()
     @State var titleInput = ""
     @State var contentInput = ""
+    @State var image: UIImage?
+    @State var showImagePicker: Bool = false
     
     var body: some View {
         VStack {
             // 存储新笔记
             VStack {
-                TextField("标题", text: $titleInput)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-                TextField("正文", text: $contentInput)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
                 HStack {
+                    if image != nil {
+                        Image(uiImage: image!)
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .frame(width: 70, height: 70)
+                            .cornerRadius(100)
+                    }
+                    VStack {
+                        TextField("标题", text: $titleInput)
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                        TextField("正文", text: $contentInput)
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                    }
+                }
+                Divider()
+                HStack {
+                    Button(action: {
+                        showImagePicker.toggle()
+                    }) {
+                        RoundButton(text: "图片", image: "photo")
+                    }
+                    .sheet(isPresented: $showImagePicker) {
+                        ImagePickerView(sourceType: .photoLibrary) { image in
+                            self.image = image
+                        }
+                    }
                     Spacer()
                     Button(action: {
-                        let id = UUID()
-                        let newNote = Note(id: id, title: titleInput, content: contentInput)
-                        noteData.notes.append(newNote)
-                        noteData.saveNotes()
-                        titleInput = ""
-                        contentInput = ""
+                        save()
                     }) {
                         RoundButton(text: "储存笔记", image: "note.text.badge.plus")
                     }
@@ -43,17 +63,40 @@ struct NoteEntryAndList: View {
             // 读取现有笔记
             List {
                 ForEach(noteData.notes) { note in
-                    VStack(alignment: .leading, spacing: 10) {
-                        Text(note.title)
-                            .font(.title3)
-                            .bold()
-                        Text(note.content)
+                    HStack {
+                        if note.imageURLAppendix != nil {
+                            Image(uiImage: noteData.getImage(note.imageURLAppendix!))
+                                .resizable()
+                                .aspectRatio(contentMode: .fill)
+                                .frame(width: 60, height: 60)
+                                .cornerRadius(100)
+                        }
+                        VStack(alignment: .leading, spacing: 10) {
+                            Text(note.title)
+                                .font(.title3)
+                                .bold()
+                            Text(note.content)
+                        }
+                        .padding()
                     }
                 }
                 .onDelete(perform: delete)
             }
             .listStyle(InsetGroupedListStyle())
         }
+    }
+    
+    func save() {
+        let id = UUID()
+        if let data = image?.pngData() {
+            noteData.saveImage(id: id, data: data)
+        }
+        let newNote = Note(id: id, title: titleInput, content: contentInput, imageURLAppendix: image == nil ? nil : "\(id).png")
+        noteData.notes.append(newNote)
+        noteData.saveNotes()
+        titleInput = ""
+        contentInput = ""
+        image = nil
     }
     
     func delete(at offsets: IndexSet) {
